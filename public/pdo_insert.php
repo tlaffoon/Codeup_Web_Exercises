f44ee8336ce428af0685af2c87a604df88f0d44a
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2014-06-24 08:49:53 -0500

    Refactored to use pdo bind values, sanitizing input and preparing queries prior to insertion.

--- public/pdo_insert.php
+++ public/pdo_insert.php
@@ -7,69 +7,39 @@ $dbc = new PDO('mysql: host=127.0.0.1; dbname=test', 'codeup', 'password');
 $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
-// Create a new file called pdo_insert.php and migrate your connection code 
-// from php_pdo.php to it. Use PDO for all queries, nothing should be done 
-// directly in MySQL or Sequel Pro. Between each step commit your changes to Git. 
-// At the end the exercise, push all your commits to GitHub.
-
-// For these exercises, we will be creating a database that contains national parks 
-// from this list: http://en.wikipedia.org/wiki/List_of_national_parks_of_the_United_States
-
-// Create a new table named national_parks with the following fields:
-
-// id (primary key, auto increment)
-// name
-// location
-// date_established
-// area_in_acres
 
 //  national_parks | CREATE TABLE `national_parks` (
 //   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
 //   `name` varchar(99) DEFAULT NULL,
 //   `location` varchar(99) DEFAULT NULL,
 //   `date_established` date DEFAULT NULL,
-//   `area_in_acres` int(10) DEFAULT NULL,
+//   `acreage` int(10) DEFAULT NULL,
+//   `comments` text
 //   PRIMARY KEY (`id`)
 // ) ENGINE=InnoDB DEFAULT CHARSET=latin1
 
-// For each field, determine the best data type and length. Do not worry about the park description at this time.
-
-// Insert the name, location, date established, and area in acres for 10 parks listed on the Wikipedia page linked above. 
-// Using an array is a good idea.
-
-// Acadia
-// Maine
-// 44.35°N 68.21°W	
-// February 26, 1919	
-// 47,389.67 acres (191.8 km2)	
-
-
-// Open parks.csv and populate data array with contents.
-
+// Open parks.csv and populate entries array with contents.
 $rows = array_map('str_getcsv', file('parks.csv'));
 $header = array_shift($rows);
-$data = array();
-foreach ($rows as $row) {
-  $data[] = array_combine($header, $row);
-}
-
-var_dump($data);
-
-// Iterate through data entries and insert into mysql database.
-
-foreach ($data as $entry) {
-	$string = '';
-	foreach ($entry as $key => $value) {	
-		// Drop comma on last value for correct sql syntax.
-		if ($key == 'Acreage') {
-			$string .= "\"$value\"";
-		}
-
-		else {
-		$string .= "\"$value\", ";
-		}
+$entries = array();
+	foreach ($rows as $row) {
+		$entries[] = array_combine($header, $row);
 	}
-	//echo "<p>INSERT INTO national_parks (name, location, date_established, area_in_acres) VALUES ($string);</p>";
-	$query = "INSERT INTO national_parks (name, location, date_established, area_in_acres) VALUES ($string)";
-	$dbc->exec($query);
+
+$query = 'INSERT INTO national_parks (name, location, date_established, acreage, comments) 
+		VALUES (:name, :location, :date_established, :acreage, :comments)';
+
+$stmt = $dbc->prepare($query);
+
+var_dump($entries);
+
+// // Iterate through data entries and bind values.
+foreach ($entries as $entry) {
+	$stmt->bindValue(':name', $entry['name'], PDO::PARAM_STR);
+	$stmt->bindValue(':location',  $entry['location'],  PDO::PARAM_STR);
+	$stmt->bindValue(':date_established', $entry['date'], PDO::PARAM_STR);
+	$stmt->bindValue(':acreage',  $entry['acreage'],  PDO::PARAM_INT);
+	$stmt->bindValue(':comments', $entry['comments'], PDO::PARAM_STR);
+	$stmt->execute();
+	echo "Inserted ID: " . $dbc->lastInsertId() . PHP_EOL;
 }
