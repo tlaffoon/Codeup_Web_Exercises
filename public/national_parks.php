32f77df5d6e7e9c5c1f038e8e6f2a7210bac2ed6
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2014-06-24 09:36:10 -0500

    Added form for data submission, included try/catch for exceptions on blank post data.  Bound values for insertion into mysql database for form submissions.  Additional refactoring necessary to standardize mysql queries to utilize PDO.

--- public/national_parks.php
+++ public/national_parks.php
@@ -1,49 +1,53 @@
 <?php 
 
-// Use PDO for all queries, nothing should be done directly in MySQL or Sequel Pro. 
-// Between each step commit your changes to Git. At the end the exercise, push all your commits to GitHub.
+// Update your table definition to include the park description. Update your insert statements to use $dbc->prepare() and bound variables. You may still use $dbc->exec() to create the table.
 
-// Create a page that lists the national parks from your database. On each page load, 
-// it should retrieve all records from the database and display them.
+// Update the query(s) in national_parks.php to use prepared statements, in particular for the limit and offset.
 
-// Modify your page to only load four parks at a time and add links to go the next or previous pages.
+// Add a form that allows national parks to be added to your database. Use prepared statements for all queries that contain dynamic data.
 
-// Modify your query to load the appropriate parks given which page the user is on. 
-// You should accept one or two parameters from $_GET and use them to load the appropriate parks.
+// Validate all form inputs to ensure that database entries are not empty.
 
 $dbc = new PDO('mysql: host=127.0.0.1; dbname=test', 'codeup', 'password');
 
 // Tell PDO to throw exceptions on error
 $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
 
-// echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
+// ------------------------------------------------------------------- //
 
-//$stmt = $dbc->query('SELECT count(*) FROM national_parks;');
-//$numParks = $stmt->fetchColumn();
-//echo 'There are ' . $numParks . ' parks in our database.' . PHP_EOL;
+$limit = 5;
 
-if (empty($_GET)) {
-	$query = ('SELECT * FROM national_parks LIMIT 4;');
+// change to ternary
+if (!empty($_GET)) {
+	$pageID = $_GET['page'];
+}
+
+else {
 	$pageID = 0;
 }
 
-if (!empty($_GET)) {
-	//var_dump($_GET);
-	$query = ("SELECT * FROM national_parks LIMIT 4 OFFSET " . ($_GET['page'] * 4) . ";");
-	//echo "$query";
-	$pageID = $_GET['page'];
-	//echo "$pageID";
-}
-
-
+$query = ("SELECT * FROM national_parks LIMIT " . $limit . " OFFSET " . ($pageID * 4)  . ";");
 
 $stmt = $dbc->query($query);
+
+$count = $dbc->query('SELECT COUNT(*) FROM national_parks;')->fetchColumn();
+
+echo "$count";
+
+$numPages = ceil($count / $limit);
+echo "$numPages";
+//echo "$count";
+//echo "$numPages";
+
+// ------------------------------------------------------------------- //
 
 ?>
 
 <html>
 <head>
 	<title>National Parks</title>
+	<link rel="stylesheet" type="text/css" href="./bootstrap/css/bootstrap.css">
 	<style type="text/css">
 	.nav {
 		width: 400px;
@@ -51,12 +55,16 @@ $stmt = $dbc->query($query);
 	</style>
 </head>
 <body>
-  <table>
+
+<div class="container">
+
+  <table class="table table-striped">
 	<tr>
 		<th>Name</th>
 		<th>Location</th>
 		<th>Date Established</th>
-		<th>Area in Acres</th>
+		<th>Acreage</th>
+		<th>Comments</th>
 	</tr>
 
 	<tr>
@@ -64,19 +72,86 @@ $stmt = $dbc->query($query);
 			echo "<td>{$row['name']}</td>";
 			echo "<td>{$row['location']}</td>";
 			echo "<td>{$row['date_established']}</td>";
-			echo "<td>{$row['area_in_acres']}</td></tr>";
+			echo "<td>{$row['acreage']}</td>";
+			echo "<td>{$row['comments']}</td></tr>";
 		}?>
   </table>
-
-  <div class="nav">
 
   <? if ($pageID != 0) : ?>
   	<a style="float: left" href="?page=<?= ($pageID - 1) ?>">Previous</a>
   <? endif ?>
   
-  <a style="float: right" href="?page=<?= ($pageID + 1) ?>">Next</a>
+  <? if ($pageID < $numPages) : ?>
+  	<a style="float: right" href="?page=<?= ($pageID + 1) ?>">Next</a>
+  <? endif ?>
+  
+</div>
 
-  </div>
+<div class="container">
+
+<?php
+
+if (!empty($_POST)) {
+	try {
+			foreach ($_POST as $key => $value) {
+				if ($value == '') {
+					throw new Exception("Please include a valid " . ucfirst($key) . ".", 1);
+				}
+			}
+
+		$formData = array();
+		$formData[] = $_POST;
+	
+		var_dump($formData);
+
+		$query = 'INSERT INTO national_parks (name, location, date_established, acreage, comments) 
+					VALUES (:name, :location, :date_established, :acreage, :comments)';
+
+		$stmt = $dbc->prepare($query);
+
+		foreach ($formData as $data) {
+			$stmt->bindValue(':name', $data['name'], PDO::PARAM_STR);
+			$stmt->bindValue(':location',  $data['location'],  PDO::PARAM_STR);
+			$stmt->bindValue(':date_established', $data['date'], PDO::PARAM_STR);
+			$stmt->bindValue(':acreage',  $data['acreage'],  PDO::PARAM_INT);
+			$stmt->bindValue(':comments', $data['comments'], PDO::PARAM_STR);
+			$stmt->execute();
+			echo "<p>Inserted ID: " . $dbc->lastInsertId() . "</p>";
+		}
+	} 
+
+	catch (Exception $e) {
+		echo $e->getMessage();
+	}
+}
+
+?>
+
+	<form role="form" method="POST" enctype="multipart/form-data" action="">
+	  <h2>Add a Park!</h2>
+		<div class="form-group">
+
+			<label for="name">Name:</label>
+			<input class="form-control" id="name" name="name" type="text" placeholder="">
+
+			<label for="location">Location</label>
+			<input class="form-control" id="location" name="location" type="text" placeholder="">
+
+			<label for="date">Date Established</label>
+			<input class="form-control" id="date" name="date" type="date" placeholder="">
+
+			<label for="acreage">Acreage</label>
+			<input class="form-control" id="acreage" name="acreage" type="text" placeholder="">
+
+			<label for="comments">Comments</label>
+			<input class="form-control" id="comments" name="comments" type="text" placeholder="">
+
+		</div>
+		
+		<button class="btn btn-default" type="submit" value="submit">Add</button>
+	</form>
+
+</div>
 
 </body>
 </html>
